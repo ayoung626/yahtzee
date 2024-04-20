96ca08be174f492bb74042c2696df676b2a277df
Andrew Young <andrewyoung626@gmail.com>
2024-04-19 18:41:56 -0600

    Made strategy1 more self-contained

--- index.py
+++ index.py
@@ -8,10 +8,11 @@ import strategies as strat
 
 start_time = time.time()
 strategylist = [strat.strategy1]
+resultlist = []
 for s in strategylist:
     random.seed = 12345
     finalscoreslist = []
-    for _ in range(10000):
+    for _ in range(1000):
         scoringdict = {'Y':None,
                        'FH':None,
                        'LS':None,
@@ -26,10 +27,7 @@ for s in strategylist:
                        '5':None,
                        '6':None}
         for i in range(13):
-            #TO FIX
-            dicedict, categorydict = bf.scoredice(bf.diceroll())
-            s(dicedict,categorydict, scoringdict)
-            #END OF TO FIX
+            s(scoringdict)
         totalscore = 0
         uppersectionscore = 0
         for k,v in scoringdict.items():
@@ -39,7 +37,10 @@ for s in strategylist:
         if uppersectionscore>=63:
             totalscore+=35
         finalscoreslist.append(totalscore)
-    print(str(s).split(" ")[1])
-    print(stat.mean(finalscoreslist))
-    print(stat.stdev(finalscoreslist))
+    resultlist.append((str(s).split(" ")[1], stat.mean(finalscoreslist), stat.stdev(finalscoreslist)))
+    #print(str(s).split(" ")[1])
+    #print(stat.mean(finalscoreslist))
+    #print(stat.stdev(finalscoreslist))
+for r in resultlist:
+    print(r)
 print("%s seconds" % round((time.time() - start_time), 2))
--- strategies.py
+++ strategies.py
@@ -1,4 +1,7 @@
-def strategy1(dicedict, categorydict, scoringdict, current_turn = 0):
+import basicfunctions as bf
+
+def strategy1(scoringdict, current_turn = 0):
+    dicedict, categorydict = bf.scoredice(bf.diceroll())
     highestscore = 0
     highestkey = ""
     for k,v in categorydict.items():
